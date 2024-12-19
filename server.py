1ab0e2fad8a9c6331f6720c3b479a6e8b7d79edd
Trevor <66954982+TremorIO@users.noreply.github.com>
2024-12-19 22:47:17 +0300

    Fix: Deleted unnecessary tracked files and reworked the authentication process to fix an issue that prevented one to login or sign up. Bonus: Removed the issue where unique usernames could be claimed essentially logging out the original owner of the username

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField
@@ -7,17 +7,23 @@ from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import random
+from playsound import playsound
+from gtts import gTTS
 import Chatbot
+import os
+
+base_directory = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fourwordsalluppercase!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///'+ os.path.join(base_directory, 'database.db')
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'forms'
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,11 +41,18 @@ class LoginForm(FlaskForm):
     remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
-    #email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
-
+def login_authentication_thingy(form):
+   user = User.query.filter_by(username=form.username.data).first()
+   if user:
+        if check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('dashboard'))
+        return '<h1>Invalid username or password</h1>'
 
 voice = True
 
@@ -51,38 +64,34 @@ def home():
 def comm():
   return render_template('comm.html')
 
-@app.route('/forms')
+@app.route('/forms', methods=["POST", "GET"])
 def forms():
   logging = LoginForm()
   signing = RegisterForm()
+  
+  if request.method == "GET":
+      return render_template('forms.html', logging=logging, signing=signing)
 
-  return render_template('forms.html', logging=logging, signing=signing)
+  action = request.form.get("authenticate")
 
-@app.route('/login-query', methods=['GET', 'POST'])
-def login_query():
+  if action == "register":
+    if signing.validate_on_submit():
+        duplicate_user = User.query.filter_by(username=signing.username.data).first()
+        if not duplicate_user:
+            hashed_password = generate_password_hash(signing.password.data, method='pbkdf2:sha256')
+            new_user = User(username=signing.username.data, email=signing.email.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return login_authentication_thingy(signing)
+        flash("Username already exists", category="failure")
+        return render_template('forms.html', logging=logging, signing=signing)
+            
 
-  if logging.validate_on_submit():
-      user = User.query.filter_by(username=form.username.data).first()
-      if user:
-          if check_password_hash(user.password, form.password.data):
-              login_user(user, remember=form.remember.data)
-              return redirect(url_for('dashboard'))
+  elif action == "login":
+    if logging.validate_on_submit():
+        return login_authentication_thingy(logging)
 
-      return '<h1>Invalid username or password</h1>'
-
-  return redirect(url_for("user"))
-
-@app.route('/signup-query', methods=['GET', 'POST'])
-def signup():
-  if signing.validate_on_submit():
-      hashed_password = generate_password_hash(signing.password.data, method='sha256')
-      new_user = User(username=signing.username.data, email=signing.email.data, password=hashed_password)
-      db.session.add(new_user)
-      db.session.commit()
-
-      return '<h1>New user has been created!</h1>'
-
-  return redirect(url_for("forms"))
+    return render_template('forms.html', logging=logging, signing=signing)
 
 
 
@@ -100,7 +109,7 @@ def news_query():
 
 
 @app.route('/dashboard')
-#@login_required
+@login_required
 def dashboard():
     return render_template('dashboard.html')
 
@@ -108,7 +117,7 @@ def dashboard():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('forms'))
 
 @app.route('/chat')
 def chat():
@@ -128,6 +137,6 @@ def summary():
 if __name__ == '__main__':
   app.debug = True
   app.run( # Starts the site
-		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
-		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+		host='0.0.0.0',  
+		port=8000
 	)
